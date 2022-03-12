2a3b21de934797abaa27c5b608b45444e7b9af80
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-03-12 08:14:30 +0300

    Feat : header changeble

--- templates/header.php
+++ templates/header.php
@@ -5,10 +5,22 @@
         </div>
         <div class=''>
             <ul class="flex">
+                <?php
+                if (isset($_SERVER['PHP_SELF']) && $_SERVER['PHP_SELF'] == '/e-wallet/cart.php') {
+                ?>
+                <a href="index.php"
+                    class=" text-white font-bold flex justify-center items-center mx-1 w-fit h-12 cursor-pointer bg-primary_blue hover:bg-blue-400 p-2 px-3 rounded-full border-4 border-white transition-all">
+                    Back to shop
+                </a>
+                <?php
+                } else {
+                ?>
                 <a href="cart.php"
                     class=" flex justify-center items-center mx-3 w-12 h-12 cursor-pointer bg-primary_blue hover:bg-blue-400 p-2 rounded-full border-4 border-white transition-all">
                     <ion-icon class="text-white text-xl" name="bag-handle-outline"></ion-icon>
                 </a>
+                <?php
+                } ?>
                 <li
                     class=" flex justify-center items-center mx-1 w-fit h-12 cursor-pointer bg-primary_blue hover:bg-blue-400 p-2 px-3 rounded-full border-4 border-white transition-all">
                     <a class="text-white font-bold">
